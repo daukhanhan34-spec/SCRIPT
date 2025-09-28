e66ccae3b3127923e38a72ef78cc5e20a16ac29e
daukhanhan34-spec <daukhanhan34@gmail.com>
2025-09-28 10:16:43 +0700

    Create 1001.lua

---
+++ 1001.lua
@@ -0,0 +1,7 @@
+getgenv().hop = "old"
+getgenv().reducelag = true
+getgenv().blackscreen = false
+getgenv().AutoFarm = false
+getgenv().autoexecuter = true
+getgenv().WebhookURL = "" --If not used do not write anything
+loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameFarmdiamond.lua"))()
